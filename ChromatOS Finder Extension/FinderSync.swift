e2ebc7e94a6de32261db3401c9df64af8a4a02ec
Judit Karamazov <125817426+JuditKaramazov@users.noreply.github.com>
2023-11-05 20:41:01 +0100

    Add FinderSync.swift

---
+++ ChromatOS Finder Extension/FinderSync.swift
@@ -0,0 +1,69 @@
+//
+//  FinderSync.swift
+//  ChromatOS Finder Extension.
+//
+//  @JuditKaramazov, 2023.
+//
+
+import Cocoa
+import FinderSync
+
+class FinderSync: FIFinderSync {
+    
+    private let extendedAttributeName = "com.JuditKaramazov.ChromatOS.FolderColor"
+
+    override init() {
+        super.init()
+        
+        FIFinderSyncController.default().directoryURLs = [URL(fileURLWithPath: "/")]
+    }
+        
+    override func menu(for menu: FIMenuKind) -> NSMenu? {
+        guard case .contextualMenuForItems = menu,
+              let selectedItemURLs = FIFinderSyncController.default().selectedItemURLs(),
+              selectedItemURLs.filter({ !$0.hasDirectoryPath }).isEmpty
+        else { return nil }
+        
+        let currentColorRawValues = Set(selectedItemURLs
+            .map { try? $0.extendedAttribute(forName: extendedAttributeName) }
+            .map { data in data.map { String(data: $0, encoding: .utf8)! } })
+        
+        let menu = NSMenu(title: "")
+        let rootItem = NSMenuItem(title: String(localized: "Color"), action: nil, keyEquivalent: "")
+        let submenu = NSMenu(title: "")
+        
+        let submenuItems = Color.allCases.map { color -> NSMenuItem in
+            let menuItem = NSMenuItem(title: color.localizedName, action: #selector(handleMenuItem(_:)), keyEquivalent: "")
+            menuItem.image = NSImage(named: color.menuImageName)
+            if currentColorRawValues.count == 1 {
+                let currentColor = currentColorRawValues[currentColorRawValues.startIndex]
+                if currentColor == color.rawValue || currentColor == nil && color == .blue { menuItem.state = .on }
+            }
+            return menuItem
+        }
+        
+        submenu.items = submenuItems
+        rootItem.submenu = submenu
+        menu.addItem(rootItem)
+        return menu
+    }
+    
+    @objc private func handleMenuItem(_ sender: NSMenuItem) {
+        let selectedItemURLs = FIFinderSyncController.default().selectedItemURLs()
+        
+        for itemURL in selectedItemURLs! {
+            let color = Color.allCases.first { $0.localizedName == sender.title }!
+            var image: NSImage?
+            if color != .blue { image = NSImage(named: color.rawValue) }
+            
+            if NSWorkspace.shared.setIcon(image, forFile: itemURL.path, options: []) {
+                if image == nil {
+                    try? itemURL.removeExtendedAttribute(forName: extendedAttributeName)
+                } else {
+                    try? itemURL.setExtendedAttribute(data: color.rawValue.data(using: .utf8)!, forName: extendedAttributeName)
+                }
+            }
+        }
+    }
+    
+}
