4b4b129a517099603eff384db465b149655d0113
Judit Karamazov <125817426+JuditKaramazov@users.noreply.github.com>
2023-11-05 20:41:15 +0100

    Add URL+XATTR.swift

---
+++ ChromatOS Finder Extension/URL+XATTR.swift
@@ -0,0 +1,48 @@
+//
+//  URL+XATTR.swift
+//  ChromatOS Finder Extension.
+//
+//  @JuditKaramazov, 2023.
+//
+
+import Foundation
+
+extension URL {
+    
+    func extendedAttribute(forName name: String) throws -> Data  {
+        let data = try self.withUnsafeFileSystemRepresentation { fileSystemPath -> Data in
+            let length = getxattr(fileSystemPath, name, nil, 0, 0, 0)
+            guard length >= 0 else { throw URL.posixError(errno) }
+            
+            var data = Data(count: length)
+            
+            let result =  data.withUnsafeMutableBytes { [count = data.count] in
+                getxattr(fileSystemPath, name, $0.baseAddress, count, 0, 0)
+            }
+            guard result >= 0 else { throw URL.posixError(errno) }
+            return data
+        }
+        return data
+    }
+    
+    func setExtendedAttribute(data: Data, forName name: String) throws {
+        try self.withUnsafeFileSystemRepresentation { fileSystemPath in
+            let result = data.withUnsafeBytes {
+                setxattr(fileSystemPath, name, $0.baseAddress, data.count, 0, 0)
+            }
+            guard result >= 0 else { throw URL.posixError(errno) }
+        }
+    }
+    
+    func removeExtendedAttribute(forName name: String) throws {
+        try self.withUnsafeFileSystemRepresentation { fileSystemPath in
+            let result = removexattr(fileSystemPath, name, 0)
+            guard result >= 0 else { throw URL.posixError(errno) }
+        }
+    }
+    
+    /// Helper function to create an NSError from a Unix errno.
+    private static func posixError(_ err: Int32) -> NSError {
+        return NSError(domain: NSPOSIXErrorDomain, code: Int(err), userInfo: [NSLocalizedDescriptionKey: String(cString: strerror(err))])
+    }
+}
